0a16b5e7ed21683dcbca2d4820bea3e332cfafd6
anshita20dubey <anshita20dubey@gmail.com>
2025-07-10 17:18:08 +0530

    fixes

--- src/Socket.js
+++ src/Socket.js
@@ -1,11 +1,13 @@
-import {io} from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-export const initSocket = async () =>{
+export const initSocket = async () => {
     const options = {
         'force new connection': true,
-        reconnectionAttempts : 'Infinity',
+        reconnectionAttempts: 'Infinity',
         timeout: 10000,
-        transports: ['websocket'],
+        transports: ['websocket', 'polling'], // Add polling as fallback
+        secure: true, // Add this for HTTPS
+        rejectUnauthorized: false // Only for development if you have SSL issues
     };
     return io(process.env.REACT_APP_BACKEND_URL, options);
 }
